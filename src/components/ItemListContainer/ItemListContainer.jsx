69f33a4536708edfc0ac72739a145f51d5c03ab1
ssantiago90 <salkin.santiago@gmail.com>
2022-11-19 15:57:03 -0300

    pre-entrega completa

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ItemListContainer() {
+  return (
+    <div>Mostrar una prop "greeting"</div>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <h2>Titulo</h2>
+      <ul>
+        <li>Categoria A</li>
+        <li>Categoria B</li>
+        <li>Categoria C</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
