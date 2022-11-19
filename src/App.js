fa6824bac5a7e35101d7e3d92c51e7d4c2ea338a
ssantiago90 <salkin.santiago@gmail.com>
2022-11-19 12:29:11 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+/* JSX */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hola Comisión!</p>
+        <p>Single Page Applicaction</p>
         <a
           className="App-link"
           href="https://reactjs.org"
